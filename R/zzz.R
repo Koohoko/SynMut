c74e36472f21797a6c5dd0ffb6b7abe9356f848b
Koohoko <Koohoko@gmail.com>
2019-04-16 16:14:39 +0800

    updated description

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,9 @@
 .onAttach <- function(libname, pkgname) {
     packageStartupMessage(
-        "SynMut: Tools for designing Synonymous Mutants from DNA Sequences
+        "
 -------------------------------------------------------------------------------
 For disccussion, please access to: https://github.com/Koohoko/SynMut/issues
--------------------------------------------------------------------------------"
+-------------------------------------------------------------------------------
+        "
     )
 }
